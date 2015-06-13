b5ad6c76a3e708d05ec1dc5f012227b8222f489b
Richard Penman <richard@webscraping.com>
2015-06-13 18:02:39 +0800

    migrated face_tracker from deprecated cv to cv2

--- face_tracker.py
+++ face_tracker.py
@@ -4,7 +4,7 @@ import time
 import math
 import datetime
 import serial
-import cv
+import cv2
 
 # Parameters for haar detection
 # From the API:
@@ -14,11 +14,11 @@ import cv
 # scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING,
 # min_size=<minimum possible face size
 
-min_size = (20,20)
-image_scale = 2
-haar_scale = 1.2
+min_size = (30,30)
+image_scale = 0.5
+haar_scale = 1.3
 min_neighbors = 4
-haar_flags = 0
+haar_flags = 0#cv2.CASCADE_SCALE_IMAGE)
 
 # For OpenCV image display
 WINDOW_NAME = 'FaceTracker'
@@ -29,59 +29,55 @@ def track(img, threshold=100):
                 (-1,-1) if no centroid was found
                 None if user hit ESC
     '''
-    cascade = cv.Load("haarcascade_frontalface_alt_tree.xml")
-    #cascade = cv.Load("haarcascade_frontalface_default.xml")
-    gray = cv.CreateImage((img.width,img.height), 8, 1)
-    small_img = cv.CreateImage((cv.Round(img.width / image_scale),cv.Round (img.height / image_scale)), 8, 1)
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
     # convert color input image to grayscale
-    cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
-
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # scale input image for faster processing
-    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
-
-    cv.EqualizeHist(small_img, small_img)
+    small_img = cv2.resize(gray, (0,0), fx=image_scale, fy=image_scale) 
+    small_img = cv2.equalizeHist(small_img)
 
     center = (-1,-1,-1)
     if(cascade):
-        t = cv.GetTickCount()
+        t = cv2.getTickCount()
         # HaarDetectObjects takes 0.02s
-        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0), haar_scale, min_neighbors, haar_flags, min_size)
-        t = cv.GetTickCount() - t
-        if faces:
-            faces.sort()
-            ((x, y, w, h), n) = faces[-1]
-            # the input to cv.HaarDetectObjects was resized, so scale the
-            # bounding box of each face and convert it to two CvPoints
-            pt1 = (int(x * image_scale), int(y * image_scale))
-            pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
-            cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
-            #cv.Rectangle(img, (x,y), (x+w,y+h), 255)
+        faces = cascade.detectMultiScale(small_img, scaleFactor=haar_scale, minNeighbors=min_neighbors, minSize=min_size, flags=haar_flags)
+
+        t = cv2.getTickCount() - t
+        if len(faces) > 0:
+            x1, y1, w, h = faces[0]
+            x2 = x1 + w
+            y2 = y1 + h
+            # the input to Haar was resized, so scale the
+            # bounding box of each face and convert it to two Points
+            pt1 = (int(x1 / image_scale), int(y1 / image_scale))
+            pt2 = (int(x2 / image_scale), int(y2 / image_scale))
+            cv2.rectangle(img, pt1, pt2, (255, 0, 0), 2)
+
             # get the xy corner co-ords, calc the center location
-            x1 = pt1[0]
-            x2 = pt2[0]
-            y1 = pt1[1]
-            y2 = pt2[1]
             centerx = x1+((x2-x1)/2)
             centery = y1+((y2-y1)/2)
+            h = y2 - y1
             center = (centerx, centery, h*3)
 
     if cmp(center, (-1,-1,-1)) == 0:
         center = None
 
-    cv.NamedWindow(WINDOW_NAME, 1)
-    cv.ShowImage(WINDOW_NAME, img)
+    cv2.imshow(WINDOW_NAME, img)
 
-    if cv.WaitKey(5) == 27:
+    if cv2.waitKey(1) & 0xFF == 27:
         center = None
     return center
 
+
 if __name__ == '__main__':
+    cam = cv2.VideoCapture(0)
+    running = True
 
-    capture = cv.CaptureFromCAM(0)
-
-    while True:
-
-        if not track(cv.QueryFrame(capture)):
+    while running:
+        running, img = cam.read()
+        if track(img) is None:
             break
 
+    cam.release()
+    cv2.destroyAllWindows()
